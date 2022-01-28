8d0d27a779ed8921164521ed4e42ea0c73e4c8a0
Shehel Yoosuf <shehel@pop-os.localdomain>
2022-01-28 23:35:35 +0300

    sync

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,29 +2,47 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import argparse
+import glob
 
+from clearml import Task
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+def main():
+    """ Runs data processing scripts to turn raw data from input path into
+        a subset ready for quick training saved in output path.
     """
+
+    #task = Task.init(project_name='t4c', task_name='subset_creation')
+
+    parser = argparse.ArgumentParser(description="T4C data subset creation")
+    parser.add_argument('-i', '--input_path',type=str, required=True, help='path to the raw full dataset')
+    parser.add_argument('-o', '--output_path',type=str, required=True, help='path to where the subset will be stored', default=Path('../data/.raw/'))
+    parser.add_argument('-t', '--train_count',type=int, help='Count of training samples from each map', default=10)
+    parser.add_argument('-v', '--val_count',type=int, help='Count of training samples from each map', default=10)
+    parser.add_argument('-s', '--seed',type=int, help='random seed (default:1)', default=1)
+
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    args = parser.parse_args()
+    print (args)
+    cities = ['ANTWERP', 'BANGKOK', 'BARCELONA', 'BERLIN', 'CHICAGO', 'ISTANBUL', 'MELBOURNE', 'MOSCOW', 'NEWYORK', 'VIENNA']
+
+    files = []
+    for city in cities:
+        for file in glob.glob(args.input_path + '/'+ city + '/training/*'):
+            files.append(file)
+        print (len(files))
+        break
+
+
+
+
+
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
